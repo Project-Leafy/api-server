d0e41b0588980f2eccb9cbe6c022ea6cab03ad61
gicks04 <gichanyoon@gmail.com>
2025-10-31 22:45:34 +0900

    feat: 기본환경 설정

---
+++ src/main/java/com/leafy/LeafyApplication.java
@@ -0,0 +1,13 @@
+package com.leafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafyApplication.class, args);
+	}
+
+}
